02a8a14ca5285e98d830ff79f268e56c36881c17
Dan Toulcher <dtoulcher@companieshouse.gov.uk>
2025-05-13 11:34:29 +0100

    [JU-1696] feat: update to remove test execcution when packaging

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
-# Start script for company-appointment-consumer
-exec java -jar -Dserver.port="${PORT}" "company-appointment-consumer.jar"
+# Start script for company-appointments-consumer
+exec java -jar "company-appointments-consumer.jar"
